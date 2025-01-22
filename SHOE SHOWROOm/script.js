b04e6afe84930a064b39bff0f9307f0b72a000d2
saadsindhi7861 <159277085+saadsindhi7861@users.noreply.github.com>
2025-01-22 16:47:59 +0530

    Delete SHOE SHOWROOm directory

--- SHOE SHOWROOm/script.js
+++
@@ -1,9 +0,0 @@
-// Hamburger Menu Toggle
-const hamburgerMenu = document.getElementById("hamburger-menu");
-const navList = document.querySelector(".nav-list");
-
-hamburgerMenu.addEventListener("click", () => {
-    navList.classList.toggle("active");
-    hamburgerMenu.classList.toggle("active");
-});
-
